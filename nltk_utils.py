117e2beab88bb10ec9db499b8a8e913da055b145
mohammedshaneeb-ai <shaneebkottakkal@gmail.com>
2023-07-07 10:51:32 +0530

    json file added

---
+++ nltk_utils.py
@@ -0,0 +1,21 @@
+# Importing Libraries
+import nltk
+nltk.download('punkt')
+from nltk.stem.porter import PorterStemmer
+
+
+stemmer = PorterStemmer()
+def tokenization(sentence):
+    return nltk.word_tokenize(sentence)
+
+def stem(word):
+    return stemmer.stem(word.lower())
+
+def bag_of_words(tokenized_sentence, all_words):
+    pass
+
+a = "what is your name"
+print(a)
+a = tokenization(a)
+print(a)
+
